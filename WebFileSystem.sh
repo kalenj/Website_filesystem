547d9d4878a394d1cc1d311319e12a0ce780fb86
kalen jackson <kalen.jackson@hgs.hiddengeniusproject.org>
2018-10-16 18:37:16 -0700

    first commit

---
+++ WebFileSystem.sh
@@ -0,0 +1,21 @@
+cd ~/Desktop
+mkdir Website_FileSystem
+cd Website_filesystem
+touch index.html
+mkdir html css img extras
+cd html
+touch PG1.html pg2.html
+cd ../css
+touch index.css pg1.css pg2.css
+cd ../img
+curl -o img1.png "https://dks.scene7.com/is/image/dkscdn/16SPLUNBRPLCGMBLLBKB" 
+curl -o img2.png "https://s18670.pcdn.co/wp-content/uploads/GettyImages-511947890-1.jpg"
+curl -o img3.png "http://getdrawings.com/img/juggling-silhouette-5.jpg"
+cd ../extras
+mkdir audio video
+cd audio
+touch song1.n4a
+cd ../
+cd video
+touch vid1.mp4
+
